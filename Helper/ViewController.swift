f939d943ae1a4a6aa7918d7e911dab427a6d15b2
mirageowen <ctyandmydogs@gmail.com>
2017-07-22 00:31:34 +0800

    first commit

---
+++ Helper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Helper
+//
+//  Created by owen on 17/7/22.
+//  Copyright © 2017年 owen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
