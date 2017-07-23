5998b774d5b90309f9644f48a7583f8d42ed49f7
mirageowen <ctyandmydogs@gmail.com>
2017-07-23 14:54:28 +0800

    调整buildParameter方法为Swift3.1，增加不同请求方法

---
+++ Helper/Network/Network.swift
@@ -0,0 +1,58 @@
+//
+//  Network.swift
+//  Helper
+//
+//  Created by owen on 17/7/22.
+//  Copyright © 2017年 owen. All rights reserved.
+//
+
+import Foundation
+
+public class Network {
+    
+    // basic
+    public static func request(method: String, url: String, headers: [String: String], parameters: [String: Any], completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        
+        let manager = NetworkManager(url: url, method: method, headers: headers ,parameters: parameters, completionHandler: completionHandler)
+        manager.run()
+    }
+    
+    // 不带参数
+    public static func request(method: String, url: String, headers: [String: String], completionHandler: @escaping(Data?, URLResponse?, Error?) -> Void) {
+        let manager = NetworkManager(url: url, method: method, headers: headers, completionHandler: completionHandler)
+        manager.run()
+    }
+    
+    // GET 不带参数
+    public static func get(url: String, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let manager = NetworkManager(url: url, method: "GET", completionHandler: completionHandler)
+        manager.run()
+    }
+    
+    // GET 带参数
+    public static func get(url: String, headers: [String: String], parameters: [String: Any], completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let manager = NetworkManager(url: url, method: "GET", headers: headers, parameters: parameters, completionHandler: completionHandler)
+        manager.run()
+    }
+    
+    // POST 不带参数
+    public static func post(url: String, headers: [String: String], completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let manager = NetworkManager(url: url, method: "POST", headers: headers, completionHandler: completionHandler)
+        manager.run()
+    }
+    
+    // POST 带参数
+    public static func post(url: String, headers: [String: String], paremeters: [String: Any], completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let manager = NetworkManager(url: url, method: "POST", headers: headers, parameters: paremeters, completionHandler: completionHandler)
+        manager.run()
+    }
+}
+
+
+
+
+
+
+
+
+
--- Helper/ViewController.swift
+++ Helper/ViewController.swift
@@ -13,13 +13,63 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        ///
+        //testRequestBasic()
+        //testGetWithNoParameters()
+        testGetWithParameters()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /// Examples
+    //
+    func testRequestBasic() {
+        let headers: [String: String] = [
+            "Content-Type": "application/json",
+            "seqnum": "0",
+            "ver": "1.0",
+            "uid": "498",
+            "token": "nq4LWlvy7lJW-kh07fRRuDGeBwRvpnsJ0BGl17Xe4eeZEwvXwQN8HoBAluLmJbpQ",
+            ]
+        let parameters = [
+            "data": [
+                "id": "46658",
+                "name": "free"
+            ]
+        ]
+        let url = "http://test.api.fengchaoyou.com/v1/product/detail"
+        
+        Network.request(method: "POST", url: url, headers: headers, parameters: parameters) { (data, response, error) in
+            //
+            do {
+                if let jsonData = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: AnyObject] {
+                    print("jsonData: \(String(describing: jsonData))")
+                }
+            } catch let error {
+                print("error: \(error.localizedDescription)")
+            }
+            //
+        }
 
+    }
+    
+    //
+    func testGetWithNoParameters() {
+        Network.get(url: "http://www.jianshu.com/p/fe2798574aa0") { (data, response, error) in
+            print("response: \(response)")
+        }
+    }
+
+    // 
+    func testGetWithParameters() {
+        Network.get(url: "http://music.163.com/#/my/m/music/playlist", headers: [:], parameters: ["id": "781294334", "name": "free", "type": "audio"]) { (data, response, error) in
+            print("response: \(response)")
+        }
+    }
 
 }
 
